b3b595d16b28d813c5f00dc6d440b3ad61c36d6b
panrunqiu <panrunqiu@outlook.com>
2022-11-24 12:27:39 +0800

    调试打包

--- app/src/main/java/com/peanut/nas/sdk/MainActivity.kt
+++ app/src/main/java/com/peanut/nas/sdk/MainActivity.kt
@@ -2,10 +2,12 @@ package com.peanut.nas.sdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.peanut.sdk.petlin.Extend.describeAsFileSize
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        12345678L.describeAsFileSize(" ")
     }
 }
