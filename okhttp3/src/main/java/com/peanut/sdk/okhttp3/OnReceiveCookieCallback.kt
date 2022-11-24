7cf00cebe2b6b3e0c20fb793c0cc2369f6a20ca5
panrunqiu <panrunqiu@outlook.com>
2022-11-24 22:52:33 +0800

    添加函数

--- okhttp3/src/main/java/com/peanut/sdk/okhttp3/OnReceiveCookieCallback.kt
+++ okhttp3/src/main/java/com/peanut/sdk/okhttp3/OnReceiveCookieCallback.kt
@@ -2,6 +2,7 @@ package com.peanut.sdk.okhttp3
 
 import okhttp3.Cookie
 
+@FunctionalInterface
 interface OnReceiveCookieCallback {
     fun onReceive(cookie: Cookie)
 }
--- petlin/src/main/java/com/peanut/sdk/petlin/Extend.kt
+++ petlin/src/main/java/com/peanut/sdk/petlin/Extend.kt
@@ -1,5 +1,13 @@
 package com.peanut.sdk.petlin
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Handler
+import android.util.Base64
+import android.widget.Toast
+import java.io.File
+
 object Extend{
     /**
      * 将长整型byte数描述为文件大小, 最大支持显示TB，保留两位小数。
@@ -31,4 +39,64 @@ object Extend{
             else -> "$ss$seconds"
         }
     }
+
+    /**
+     * 显示一个Toast, 支持在任意线程发送
+     * @param duration 显示时长，仅支持 [Toast.LENGTH_SHORT]或[Toast.LENGTH_LONG]
+     */
+    fun String.toast(context: Context, duration: Int = Toast.LENGTH_SHORT) {
+        if (Thread.currentThread() == context.mainLooper.thread) {
+            Toast.makeText(context, this, duration).show()
+        }else{
+            Handler(context.mainLooper).post {
+                Toast.makeText(context, this, duration).show()
+            }
+        }
+    }
+
+    /**
+     * 计算某个颜色是否为亮色，用于判断文字颜色等
+     * @param threshold 阈值，大于为亮色
+     * @return 是否为亮色，亮色应该用黑色文字
+     */
+    fun Int.isLightColor(threshold: Float = 0.5f): Boolean {
+        return try {
+            val r = this shr 16 and 0xff
+            val g = this shr 8 and 0xff
+            val b = this and 0xff
+            (0.299 * r + 0.587 * g + 0.114 * b) / 255.0
+        } catch (e: Exception) {
+            e.printStackTrace()
+            0.0
+        } > threshold
+    }
+
+    /**
+     * 复制文本到剪切板
+     */
+    fun String.copy(context: Context, label: String = context.packageName) {
+        val clipboard: ClipboardManager? = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+        val clip = ClipData.newPlainText(label, this)
+        clipboard?.setPrimaryClip(clip)
+    }
+
+    /**
+     * 使用Base 64编码文本
+     */
+    fun String.encodeBase64(vararg flags: Int): String {
+        var flag = 0
+        flags.forEach {
+            flag = flag or it
+        }
+        return Base64.encodeToString(this.toByteArray(), flag)
+    }
+
+    /**
+     * 从路径获取文件名(包含后缀)
+     */
+    fun String.getFileName() = File(this).name
+    /**
+     * 从路径获取文件名(不包含后缀)
+     */
+    fun String.getFileNameWithoutExt() = File(this).nameWithoutExtension
 }
